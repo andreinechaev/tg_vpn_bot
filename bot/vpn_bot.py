44cfcf3c43fe59d89215a33f9bf9bc41ffcfb3db
Andrei Nechaev <iphoenix179@gmail.com>
2022-03-22 21:54:33 +0100

    multi-vpn configuration

--- bot/vpn_bot.py
+++ bot/vpn_bot.py
@@ -1,3 +1,4 @@
+import os
 import traceback
 import json
 import logging
@@ -8,6 +9,9 @@ import numpy as np
 from outline.outline_vpn import OutlineVPN
 from telegram import ChatMember, Update, User, ReplyKeyboardRemove
 from telegram.ext import ConversationHandler, CallbackContext
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 VPN_URL_PREFIX = 'https://s3.amazonaws.com/outline-vpn/invite.html#'
 
@@ -20,25 +24,122 @@ def GB_to_MB(GB):
     return round(GB * 1024, 2)
 
 
+class ServersConfigurator(FileSystemEventHandler):
+
+    def __init__(self, url_path, url_filename):
+        self.logger = logging.getLogger(__name__)
+        self.url_path = url_path
+        self.url_filename = url_filename
+        self.path = os.path.join(url_path, url_filename)
+        self.servers = {}
+        self.update()
+
+    def on_modified(self, event):
+        if event.event_type == 'modified' and event.src_path == self.path:
+            with open(self.path, 'r') as f:
+                servers = json.load(f)['servers']
+                self.logger.debug(f'Loaded servers: {servers}')
+                if servers != self.servers:
+                    self.servers = servers
+                    self.logger.debug(
+                        f'Updated servers on: {event.event_type}  path : {event.src_path}')
+
+    def update(self):
+        with open(self.path, 'r') as f:
+            self.servers = json.load(f)['servers']
+
+
+class VPNProvider:
+
+    def __init__(self, vpn_urls: str):
+        self.logger = logging.getLogger(__name__)
+        self.url_path, self.url_filename = os.path.split(vpn_urls)
+        self.logger.debug(
+            f'Watching {self.url_path} for changes in {self.url_filename}')
+        self.configurator = ServersConfigurator(
+            self.url_path, self.url_filename)
+
+        self.observer = Observer()
+        self.observer.schedule(
+            self.configurator, self.url_path, recursive=False)
+        self.observer.start()
+
+    def __del__(self):
+        """
+        Destroy the observer
+        """
+        self.observer.stop()
+        self.observer.join()
+
+    def get_client(self, username: str) -> OutlineVPN:
+        """
+        Get Outline VPN client for a given username
+        :param username:
+        :return: OutlineVPN client
+
+        If user belongs to a server return OutlineVPN client for that server,
+        otherwise return the client with the least amount of users
+        """
+        if len(self.configurator.servers) == 0:
+            self.logger.error('No VPN servers found')
+            raise Exception('No VPN servers found')
+
+        vpns: list[OutlineVPN] = []
+        for server in self.configurator.servers:
+            vpn = OutlineVPN(api_url=server)
+            try:
+                for key in vpn.get_keys():
+                    if key.name == username:
+                        return vpn
+            except Exception as e:
+                self.logger.error(
+                    f'Could not connect to {server} with error: {e}')
+                continue
+
+            vpns.append(vpn)
+
+        return sorted(vpns, key=lambda vpn: len(vpn.get_keys()))[0]
+
+    def generate_url(self, username: str):
+        """
+        Generate VPN invite URL for a given username
+        :param username:
+        :return: new URL if user doesn't have a VPN, 
+        or the existing URL if user has been assigned one already
+        """
+        client = self.get_client(username)
+        if client is not None:
+            for key in client.get_keys():
+                if key.name == username:
+                    return VPN_URL_PREFIX + urllib.parse.quote(key.access_url)
+
+            new_key = client.create_key()
+            client.rename_key(new_key.key_id, username)
+            return VPN_URL_PREFIX + urllib.parse.quote(new_key.access_url)
+        else:
+            self.logger.error(f'Could not find a client for {username}')
+
+
 class VPNBot:
 
-    def __init__(self, chat_id: str, dev_chat_id: str, vpn_url: str, limit: int = 8):
+    def __init__(self, chat_id: str, dev_chat_id: str, vpn_urls: str, limit: int = 8):
+        self.logger = logging.getLogger(__name__)
         self.chat_id = chat_id
         self.dev_chat_id = dev_chat_id
-        self.vpn_url = vpn_url
         self.limit = limit
-        self.client = OutlineVPN(api_url=vpn_url)
-        self.logger = logging.getLogger(__name__)
+
+        self.provider = VPNProvider(vpn_urls)
 
     def start(self, update: Update, context: CallbackContext):
+        user = update.effective_user
         try:
             chat = context.bot.get_chat(chat_id=self.chat_id)
             member = context.bot.getChatMember(
-                chat_id=self.chat_id, user_id=update.effective_user.id)
+                chat_id=self.chat_id, user_id=user.id)
         except Exception as e:
             self.logger.error(f'Error getting chat info: {e}')
             context.bot.sendMessage(
-                chat_id=update.effective_user.id.id, text='Не удалось получить доступ к каналу; попробуйте через несколько секунд')
+                chat_id=user.id, text='Не удалось получить доступ к каналу; попробуйте через несколько секунд')
             return
 
         if not self._is_member(member):
@@ -48,29 +149,18 @@ class VPNBot:
             return
 
         self.logger.info(f'User belongs to the {chat.title}')
-        user = update.effective_user
+
         vpn_name = self._create_name(user=user)
-        vpns = self._get_vpns()
-        if vpn_name not in [vpn.name for vpn in vpns]:
-            print(f'Creating new VPN for {vpn_name}')
-            url = self._generate_vpn_url(vpn_name)
+        url = self.provider.generate_url(vpn_name)
+        if url is not None:
             self.logger.info(url)
             context.bot.sendMessage(
                 chat_id=user.id, text=f'''
                 {url} 
                 Перейдите по ссылке для дальнейших инструкций. Если вы хотите оставить отзыв, используйте команду /feedback''', protect_content=True)
         else:
-            vpns = [vpn for vpn in vpns if vpn.name == vpn_name]
-            if len(vpns) == 1:
-                self.logger.info(f'User {vpn_name} already has a VPN')
-                url = self._get_vpn_url(vpns[0])
-                self.logger.info(url)
-                context.bot.sendMessage(
-                    chat_id=user.id, text=f'''
-                {url}
-                Перейдите по ссылке для дальнейших инструкций. Если вы хотите оставить отзыв, используйте команду /feedback''', protect_content=True)
-            else:
-                self.logger.error(f'User {vpn_name} has more than one VPN')
+            context.bot.sendMessage(
+                chat_id=user.id, text=f'Невозможно создать VPN, попробуйте позднее. Если вы хотите оставить отзыв, используйте команду /feedback', protect_content=True)
 
     def start_feedback(self, update: Update, context: CallbackContext):
         try:
@@ -135,8 +225,16 @@ class VPNBot:
         user = update.effective_user
         vpn_name = self._create_name(user=user)
         self.logger.info(f'User {vpn_name} requested statistics')
-        vpns = self._get_vpns()
-        user_vpns = [vpn for vpn in vpns if vpn.name == vpn_name]
+        vpn = self.provider.get_client(vpn_name)
+        if vpn is None:
+            self.logger.error(
+                f'VPN {vpn_name} not found, server url {vpn}')
+            update.message.reply_text(
+                text='У вас нет активных VPN; Для создания нового используйте /start')
+            return
+
+        all_keys = vpn.get_keys()
+        user_vpns = [vpn for vpn in all_keys if vpn.name == vpn_name]
         if len(user_vpns) == 1:
             user_vpn: OutlineVPN = user_vpns[0]
             if user_vpn.used_bytes:
@@ -145,7 +243,7 @@ class VPNBot:
             else:
                 used_percent = 0.0
 
-            used = [vpn.used_bytes for vpn in vpns if vpn.used_bytes]
+            used = [vpn.used_bytes for vpn in all_keys if vpn.used_bytes]
             update.message.reply_text(
                 f'Вы использовали {used_percent}% трафика от {self.limit} GB.' +
                 f' Медианна/Среднестатистическое использование всех пользователей ' +
@@ -185,17 +283,6 @@ class VPNBot:
 
     def _clean_text(self, text):
         return text.replace('\n', ' ').replace('\r', '').replace('\t', ' ').strip()
-
-    def _get_vpns(self):
-        return self.client.get_keys()
-
-    def _generate_vpn_url(self, name):
-        new_key = self.client.create_key()
-        self.client.rename_key(new_key.key_id, name)
-        return VPN_URL_PREFIX + urllib.parse.quote(new_key.access_url)
-
-    def _get_vpn_url(self, vpn: OutlineVPN):
-        return VPN_URL_PREFIX + urllib.parse.quote(vpn.access_url)
 
     def _create_name(self, user: User):
         return f'{user.first_name}_{user.last_name}_{user.id}'
